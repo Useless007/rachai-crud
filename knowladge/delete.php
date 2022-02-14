b3db180ad714c5028b9ba160474d00660839c0ab
Useless007 <mammoth17216@gmail.com>
2022-02-14 21:46:24 +0700

    fix delete.php

--- knowladge/delete.php
+++ knowladge/delete.php
@@ -1,7 +1,7 @@
 <?php
     require_once __DIR__ . '/../connect.php';
     $m_id = $_GET['m_id'];
-    $sql = $conn->prepare("DELETE FROM `member` WHERE m_id = m_id");
+    $sql = $conn->prepare("DELETE FROM `member` WHERE m_id = :m_id");
     $sql->bindParam(':m_id', $m_id);
     $ai = $conn->query("ALTER TABLE `member` AUTO_INCREMENT = 1;");
     $sql->execute();
